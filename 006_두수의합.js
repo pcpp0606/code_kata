86bfc2b9694976fed14926dbffa429f8f5ff7e84
pcpp0606 <167434406+pcpp0606@users.noreply.github.com>
2024-04-26 11:14:55 +0900

    Add files via upload

---
+++ 006_두수의합.js
@@ -0,0 +1,9 @@
+function solution(num1, num2) {
+    var answer = -1;
+    answer = num1 + num2;
+    return answer;
+}
+
+console.log(solution(1,2));
+console.log(solution(100,200));
+console.log(solution(10,-20));
