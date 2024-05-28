01ec989fdc490e3d6148beb9bbaca6663eb2056a
pcpp0606 <167434406+pcpp0606@users.noreply.github.com>
2024-05-28 09:52:36 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -0,0 +1,31 @@
+let str = '';
+
+function make3(n){ // 3진법
+    if(n<3){
+        str = str+String(n);
+        return;
+    }
+    else{
+        str = str + String(n%3)
+        make3(parseInt(n/3));
+    }
+}
+
+function make10(str){ // 10진법
+    let num = 0;
+    for(let i=(str.length-1); i>=0; i--){
+        let p = str.length-i-1;
+        if(p == 0) num += Number(str[i])
+        else{
+            let n = Math.pow(3, str.length-i-1)
+            num += Number(n* str[i])
+    
+        }
+    }
+    return num;
+}
+
+function solution(n) {
+    make3(n);
+    return make10(str);
+}
