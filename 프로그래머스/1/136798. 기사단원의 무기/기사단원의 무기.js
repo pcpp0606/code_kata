90d71ca3cae6315f0bd3137ab25c30bc617768d6
pcpp0606 <167434406+pcpp0606@users.noreply.github.com>
2024-06-26 09:12:20 +0900

    [level 1] Title: 기사단원의 무기, Time: 2.10 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
@@ -1,24 +1,23 @@
 function solution(number, limit, power) {
-    var answer = 0;
-    // n 즉, 기사단원 수 만큼 반복
-    for (let n = 1; n <= number; n++){
-        let count = 0;
-        // 약수가 된다.
-        for (let j = 1; j * j <= n; j++)
-        {
-            // j의 제곱이 n이면 count를 1 증가
-            if (j * j === n)
-                count++;
-            // j가 n의 약수면 count에 +2
-            // 기본적으로 
-            else if (n % j === 0)
-                count += 2;
-        }
-        // limit과 비교하여 크다면 power값을 넣어준다.
-        if (count > limit)
-            count = power;
-        // 지금까지의 count 값을 answer에 넣어준다.
-        answer += count;
+//answer : 정답 변수, arr : 약수의 개수가 들어갈 배열 , cnt : number까지의 각 숫자의 약수의 개수
+    let answer = 0
+    let arr=Array(number)
+    let i=1
+    while(i<=number){
+    let cnt=0
+     for(let j=1;j*j<=i;j++){
+      if(i%j==0){
+       cnt++
+        if (j*j < i) 
+            cnt++
+      }
+     }
+    arr[i-1]=cnt
+    i++
     }
-    return answer;
+    arr.forEach(v=>{
+    if(v>limit)answer+=power
+    })
+    answer+=arr.filter(v=>v<=limit).reduce((a,b)=>a+b,0)
+    return answer
 }
